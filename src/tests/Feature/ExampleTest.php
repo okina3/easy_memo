93bb7cdd374a19a6c2ae6d5f6b72ca772377754c
okina3 <takosos@icloud.com>
2024-05-02 17:56:51 +0900

    テストコードのエラー修正
    ExampleTest.phpの修正完了

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -3,17 +3,22 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    // /**
-    //  * A basic test example.
-    //  */
-    // public function test_the_application_returns_a_successful_response(): void
-    // {
-    //     $response = $this->get('/');
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $user = User::factory()->create();
 
-    //     $response->assertOk();
-    // }
+        // $response = $this->get('/');
+        $response = $this->actingAs($user)
+            ->get('/');
+
+        $response->assertOk();
+    }
 }
