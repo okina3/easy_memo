d7cd2dcec0c9f9b353e889b9f0f36f3b9a35ba6e
okina3 <takosos@icloud.com>
2024-05-14 13:13:53 +0900

    管理者用の、テストコード作成。
    AuthenticationTest.phpの修正。

--- src/tests/Admin/Feature/Auth/AuthenticationTest.php
+++ src/tests/Admin/Feature/Auth/AuthenticationTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Admin\Feature\Auth;
 
-use App\Models\User;
+use App\Models\Admin;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Admin\TestCase;
@@ -17,55 +17,55 @@ class AuthenticationTest extends TestCase
      */
     public function test_login_screen_can_be_rendered(): void
     {
-        $response = $this->get('/login');
+        $response = $this->get('admin/login');
 
         $response->assertStatus(200);
     }
 
-    // /**
-    //  * ユーザーがログイン画面を使用して認証できることを確認。
-    //  * @return void
-    //  */
-    // public function test_users_can_authenticate_using_the_login_screen(): void
-    // {
-    //     $user = User::factory()->create();
+    /**
+     * ユーザーがログイン画面を使用して認証できることを確認。
+     * @return void
+     */
+    public function test_users_can_authenticate_using_the_login_screen(): void
+    {
+        $user = Admin::factory()->create();
 
-    //     $response = $this->post('/login', [
-    //         'email' => $user->email,
-    //         'password' => 'password',
-    //     ]);
+        $response = $this->post('admin/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
 
-    //     $this->assertAuthenticated();
-    //     $response->assertRedirect(RouteServiceProvider::HOME);
-    // }
+        $this->assertAuthenticated('admin');
+        $response->assertRedirect(RouteServiceProvider::ADMIN_HOME);
+    }
 
-    // /**
-    //  * 無効なパスワードでユーザーが認証できないことをテスト。
-    //  * @return void
-    //  */
-    // public function test_users_can_not_authenticate_with_invalid_password(): void
-    // {
-    //     $user = User::factory()->create();
+    /**
+     * 無効なパスワードでユーザーが認証できないことをテスト。
+     * @return void
+     */
+    public function test_users_can_not_authenticate_with_invalid_password(): void
+    {
+        $user = Admin::factory()->create();
 
-    //     $this->post('/login', [
-    //         'email' => $user->email,
-    //         'password' => 'wrong-password',
-    //     ]);
+        $this->post('admin/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
 
-    //     $this->assertGuest();
-    // }
+        $this->assertGuest('admin');
+    }
 
-    // /**
-    //  * ユーザーがログアウトできることをテスト。
-    //  * @return void
-    //  */
-    // public function test_users_can_logout(): void
-    // {
-    //     $user = User::factory()->create();
+    /**
+     * ユーザーがログアウトできることをテスト。
+     * @return void
+     */
+    public function test_users_can_logout(): void
+    {
+        $user = Admin::factory()->create();
 
-    //     $response = $this->actingAs($user)->post('/logout');
+        $response = $this->actingAs($user,'admin')->post('admin/logout');
 
-    //     $this->assertGuest();
-    //     $response->assertRedirect('/');
-    // }
+        $this->assertGuest('admin');
+        $response->assertRedirect('admin/');
+    }
 }
