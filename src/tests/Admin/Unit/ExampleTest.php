8e0cfb1e4dc549a4eef8815bfb6cb223ad6af9e3
okina3 <takosos@icloud.com>
2024-05-15 14:27:01 +0900

    管理者用の、テストコード作成。
    Admin\Feature\ExampleTest.phpの修正

--- src/tests/Admin/Unit/ExampleTest.php
+++ src/tests/Admin/Unit/ExampleTest.php
@@ -6,14 +6,14 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    // /**
-    //  * テストメソッドが常に true をアサートすることを確認。
-    //  *
-    //  *  アサート関数を使用して true が true であることをテスト。
-    //  * @return void
-    //  */
-    // public function test_that_true_is_true(): void
-    // {
-    //     $this->assertTrue(true);
-    // }
+    /**
+     * テストメソッドが常に true をアサートすることを確認。
+     *
+     *  アサート関数を使用して true が true であることをテスト。
+     * @return void
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
 }
