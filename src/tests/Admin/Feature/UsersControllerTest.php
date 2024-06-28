bb216faf8f573cedf59e509091a2ba3e843fea48
okina3 <takosos@icloud.com>
2024-06-28 21:26:52 +0900

    管理者のコントローラーのテスコード作成（UsersControllerTest）の修正

--- src/tests/Admin/Feature/UsersControllerTest.php
+++ src/tests/Admin/Feature/UsersControllerTest.php
@@ -4,14 +4,18 @@ namespace Tests\Admin\Feature;
 
 use App\Models\Admin;
 use App\Models\User;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Mockery;
 use Tests\Admin\TestCase;
 
 class UsersControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    private Admin $admin;
     private User $user;
 
     /**
@@ -23,54 +27,118 @@ class UsersControllerTest extends TestCase
         // 親クラスのsetUpメソッドを呼び出し
         parent::setUp();
         // 管理者ユーザーを作成
-        $this->admin = Admin::factory()->create();
-        // ユーザーを作成
-        $this->user = User::factory()->create();
-        // 管理者ユーザーを認証
-        $this->actingAs($this->admin, 'admin');
+        $admin = Admin::factory()->create();
+        // 管理者を認証
+        $this->actingAs($admin, 'admin');
     }
 
     /**
-     * 全ユーザー、また、検索したユーザーを表示するテスト
+     * ユーザーを作成するヘルパーメソッド
+     * @param int $count ユーザーの作成数
+     * @return Collection 作成されたユーザーのコレクション
+     */
+    private function createUsers(int $count): Collection
+    {
+        // 指定された数のユーザーを作成する
+        return User::factory()->count($count)->create();
+    }
+
+    /**
+     * 全てのユーザーの一覧表示が正しく行われることをテスト
      * @return void
      */
-    public function testIndexUsersController()
+    public function testAllIndexUsersController()
     {
-        // ユーザーを5人作成
-        User::factory()->count(5)->create();
-        // 最初のユーザーのメールアドレスをキーワードとして設定
-        $keyword = $this->user->email;
+        // 3件のユーザーを作成
+        $users = $this->createUsers(3);
 
-        // 全ユーザー、また、検索したユーザーを表示するリクエストを送信
-        $response = $this->get(route('admin.index', ['keyword' => $keyword]));
+        // 全ユーザーを表示する為に、リクエストを送信
+        $response = $this->get(route('admin.index'));
 
-        // レスポンスが正しいビューを返すことを確認
+        // レスポンスが 'admin.users.index' ビューを返すことを確認
         $response->assertStatus(200);
         $response->assertViewIs('admin.users.index');
 
         // ビューに渡されるデータが正しいか確認
-        $response->assertViewHas('all_users', function ($viewUsers) use ($keyword) {
-            return $viewUsers->contains('email', $keyword);
+        $response->assertViewHas('all_users', function ($viewUsers) use ($users) {
+            // ビューのユーザー数が3であり、かつ、ビューのユーザーと作成したユーザーの、最初のIDが一致することを確認
+            return $viewUsers->count() === 3 && $viewUsers->first()->id === $users->first()->id;
         });
     }
 
     /**
-     * ユーザーのサービス利用を停止するテスト
+     * 検索したユーザーの一覧表示が正しく行われることをテスト
+     * @return void
+     */
+    public function testSearchIndexUsersController()
+    {
+        // 3件のユーザーを作成
+        $users = $this->createUsers(3);
+
+        // 最初のユーザーのメールアドレスをキーワードとして設定
+        $keyword = $users->first()->email;
+
+        // 検索したユーザーを表示するリクエストを送信
+        $response = $this->get(route('admin.index', ['keyword' => $keyword]));
+
+        // レスポンスが 'admin.users.index' ビューを返すことを確認
+        $response->assertStatus(200);
+        $response->assertViewIs('admin.users.index');
+
+        // ビューに渡されるデータが正しいか確認
+        $response->assertViewHas('all_users', function ($viewUsers) use ($users, $keyword) {
+            // ビューから取得したユーザーをコレクションに変換
+            $viewUsers = collect($viewUsers);
+            // キーワード（メールアドレス）でユーザーを絞り込み
+            $filteredUsers = $users->filter(function ($users) use ($keyword) {
+                return stripos($users->email, $keyword) !== false;
+            });
+            // 絞り込まれたユーザーの数とIDが、ビューのユーザーと一致するかを確認
+            return $viewUsers->count() === $filteredUsers->count() &&
+                $viewUsers->pluck('id')->sort()->values()->all() === $filteredUsers->pluck('id')->sort()->values()->all();
+        });
+    }
+
+    /**
+     * ユーザーが正しくサービス利用を停止されることをテスト
      * @return void
      */
     public function testDestroyUsersController()
     {
-        // ユーザーを1人作成
-        $user = User::factory()->create();
+        // 1件のユーザーを作成
+        $user = $this->createUsers(1)->first();
 
         // ユーザーのサービス利用を停止するリクエストを送信
-        $response = $this->delete(route('admin.destroy', ['userId' => $user->id]), ['userId' => $user->id]);
+        $response = $this->delete(route('admin.destroy'), ['userId' => $user->id]);
 
         // ユーザーがソフトデリートされたことを確認
         $this->assertSoftDeleted('users', ['id' => $user->id]);
 
-        // レスポンスが正しいリダイレクト先を指していることを確認
+        // レスポンスが 'admin.index' リダイレクト先を指していることを確認
         $response->assertRedirect(route('admin.index'));
         $response->assertSessionHas(['message' => 'ユーザーのサービス利用を停止しました', 'status' => 'alert']);
+    }
+
+    /**
+     * ユーザーがサービス利用停止時のエラーハンドリングをテスト
+     * @return void
+     */
+    public function testErrorDestroyUsersController()
+    {
+        // 1件のユーザーを作成
+        $user = $this->createUsers(1)->first();
+
+        // DB::transactionメソッドが呼び出されると、一度だけ例外をスローするように設定
+        DB::shouldReceive('transaction')->once()->andThrow(new Exception('DBエラー'));
+
+        // Log::errorメソッドが呼び出されるときに、例外がログに記録されることを確認
+        Log::shouldReceive('error')->once()->with(Mockery::type(Exception::class));
+
+        // 例外がスローされることを期待し、そのメッセージが"DBエラー"であることを確認
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('DBエラー');
+
+        // ユーザーのサービス利用を停止するリクエストを送信
+        $this->delete(route('admin.destroy'), ['userId' => $user->id]);
     }
 }
