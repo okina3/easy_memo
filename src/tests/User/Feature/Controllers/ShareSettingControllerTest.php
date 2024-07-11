50f4c2ef5482e96c658c2104717a566e159bcfb0
okina3 <takosos@icloud.com>
2024-07-11 17:40:40 +0900

    コントローラーのテストコードの修正（ShareSettingControllerTest）
    ShareSettingTest.phpの不要な記述の削除、
    ShareSettingServiceTest.phpのコードの修正も含む。
    ShareSettingController.phpのコードの修正も含む。

--- src/tests/User/Feature/Controllers/ShareSettingControllerTest.php
+++ src/tests/User/Feature/Controllers/ShareSettingControllerTest.php
@@ -7,13 +7,7 @@ use App\Models\Memo;
 use App\Models\ShareSetting;
 use App\Models\Tag;
 use App\Models\User;
-use Exception;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Mockery;
 use Tests\User\TestCase;
 
 class ShareSettingControllerTest extends TestCase
@@ -23,8 +17,6 @@ class ShareSettingControllerTest extends TestCase
     private User $user;
     private User $secondaryUser;
     private User $tertiaryUser;
-    private Memo $secondaryUserMemo;
-    private Memo $tertiaryUserMemo;
 
     /**
      * テスト前の初期設定（各テストメソッドの実行前に毎回呼び出される）
@@ -43,29 +35,39 @@ class ShareSettingControllerTest extends TestCase
 
         // 認証済みのユーザーを返す
         $this->actingAs($this->user);
-
-        // ユーザーのメモ作成
-        $this->memo = Memo::factory()->create(['user_id' => $this->user->id]);
-        // 2人目の別のユーザーのメモ作成
-        $this->secondaryUserMemo = Memo::factory()->create(['user_id' => $this->secondaryUser->id]);
-        // 3人目のユーザーのメモ作成
-        $this->tertiaryUserMemo = Memo::factory()->create(['user_id' => $this->tertiaryUser->id]);
     }
 
     /**
-     * 指定されたメモを自分に共有させる設定を作成するヘルパーメソッド
-     * @param Memo $memo
-     * @return ShareSetting|Collection|Model
+     * メモを作成するヘルパーメソッド
+     * @param User $user 作成するメモに関連付けるユーザー
+     * @return Memo 作成されたメモのインスタンス
      */
-    private function createSharedMemo(Memo $memo): Model|Collection|ShareSetting
+    private function createMemo(User $user): Memo
     {
+        // メモを、現在のユーザーに関連付けて作成する
+        return Memo::factory()->create(['user_id' => $user->id]);
+    }
+
+    /**
+     * メモを共有させる設定を作成するヘルパーメソッド
+     * @param User $user メモの所有者
+     * @param User $sharingUser 共有設定させたいユーザー
+     * @param bool $editAccess 編集アクセスの許可（デフォルトは true）
+     * @return ShareSetting 作成された共有設定のインスタンス
+     */
+    private function createShareSetting(User $user, User $sharingUser, bool $editAccess = true): ShareSetting
+    {
+        // メモを作成
+        $memo = Memo::factory()->create(['user_id' => $user->id]);
+
+        // 共有設定を作成
         return ShareSetting::factory()->create([
-            // 自分に共有
-            'sharing_user_id' => $this->user->id,
-            // 別のユーザーのメモ
+            // 共有させたいユーザー
+            'sharing_user_id' => $sharingUser->id,
+            // メモの選択
             'memo_id' => $memo->id,
             // 編集も可能
-            'edit_access' => true,
+            'edit_access' => $editAccess
         ]);
     }
 
@@ -99,10 +101,12 @@ class ShareSettingControllerTest extends TestCase
      */
     public function testIndexShareSettingController()
     {
-        // 2件の他人のメモを、自分に共有する設定を作成
+        // 4件の他人のメモを、自分に共有する設定を作成
         $shareSettings = collect([
-            $this->createSharedMemo($this->secondaryUserMemo),
-            $this->createSharedMemo($this->tertiaryUserMemo)
+            $this->createShareSetting($this->secondaryUser, $this->user),
+            $this->createShareSetting($this->tertiaryUser, $this->user),
+            $this->createShareSetting($this->secondaryUser, $this->user),
+            $this->createShareSetting($this->tertiaryUser, $this->user)
         ]);
 
         // 自分に共有された他人のメモの一覧を表示する為に、リクエストを送信
@@ -113,20 +117,22 @@ class ShareSettingControllerTest extends TestCase
         $response->assertViewIs('user.shareSettings.index');
 
         // ビューに渡されるデータが正しいか確認
-        $response->assertViewHas('shared_memos', function ($viewSharedMemo) use ($shareSettings) {
-            // ビューから取得した共有されたメモをコレクションに変換
-            $viewSharedMemo = collect($viewSharedMemo);
-            // ビューに渡される共有メモが、2件、かつ、共有メモのID配列と、作成した共有設定のmemo_id配列と一致することを確認
-            return $viewSharedMemo->count() === 2 &&
-                $viewSharedMemo->pluck('id')->toArray() === $shareSettings->pluck('memo_id')->toArray();
+        $response->assertViewHas('shared_memos', function ($viewShareSettings) use ($shareSettings) {
+            // ビューから取得した共有された設定をコレクションに変換
+            $viewShareSettings = collect($viewShareSettings);
+            // ビューに渡される共有設定が、4件、かつ、
+            return $viewShareSettings->count() === 4 &&
+                // ビューに渡される共有メモのID配列と、作成した共有設定のmemo_id配列と一致することを確認
+                $viewShareSettings->pluck('id')->toArray() === $shareSettings->pluck('memo_id')->toArray();
         });
         $response->assertViewHas('shared_users', function ($viewUsers) use ($shareSettings) {
             // ビューから取得したユーザーをコレクションに変換
             $viewUsers = collect($viewUsers);
             // メモに関連するユーザーを一意に取得
             $sharedUsers = $shareSettings->pluck('memo.user')->unique();
-            // ビューに渡されるユーザーが、2件、かつ、ユーザーID配列と、作成したメモに関連するユーザーID配列と一致することを確認
+            // ビューに渡されるユーザーが、2件、かつ、
             return $viewUsers->count() === 2 &&
+                // ビューに渡されるユーザーID配列と、作成したメモに関連するユーザーID配列と一致することを確認
                 $viewUsers->pluck('id')->toArray() === $sharedUsers->pluck('id')->toArray();
         });
     }
@@ -137,10 +143,12 @@ class ShareSettingControllerTest extends TestCase
      */
     public function testStoreShareSettingController()
     {
+        // 1件の自分のメモを作成
+        $memo = $this->createMemo($this->user);
         // 1件の共有設定のデータを作成（自分のメモを、2人目の別のユーザーに共有）
         $requestData = [
             'share_user_start' => $this->secondaryUser->email,
-            'memoId' => $this->memo->id,
+            'memoId' => $memo->id,
             'edit_access' => true,
         ];
 
@@ -150,7 +158,7 @@ class ShareSettingControllerTest extends TestCase
         // 共有設定が保存されているか確認
         $this->assertDatabaseHas('share_settings', [
             'sharing_user_id' => $this->secondaryUser->id,
-            'memo_id' => $this->memo->id,
+            'memo_id' => $memo->id,
             'edit_access' => true,
         ]);
 
@@ -158,42 +166,15 @@ class ShareSettingControllerTest extends TestCase
         $response->assertRedirect(route('user.index'));
         $response->assertSessionHas(['message' => 'メモを共有しました。', 'status' => 'info']);
     }
-    // --------------------------------------------------------------------------------------------------------
-    // /**
-    //  * 自分のメモの共有設定が、正しく保存される時のエラーハンドリングをテスト
-    //  * @return void
-    //  */
-    // public function testErrorStoreShareSettingController()
-    // {
-    //     // 1件の共有設定のデータを作成（自分のメモを、2人目の別のユーザーに共有）
-    //     $requestData = [
-    //         'share_user_start' => $this->secondaryUser->email,
-    //         'memoId' => $this->memo->id,
-    //         'edit_access' => true,
-    //     ];
 
-    //     // DB::transactionメソッドが呼び出されると、一度だけ例外をスローするように設定
-    //     DB::shouldReceive('transaction')->once()->andThrow(new Exception('DBエラー'));
-
-    //     // Log::errorメソッドが呼び出されるときに、例外がログに記録されることを確認
-    //     Log::shouldReceive('error')->once()->with(Mockery::type(Exception::class));
-
-    //     // 例外がスローされることを期待し、そのメッセージが"DBエラー"であることを確認
-    //     $this->expectException(Exception::class);
-    //     $this->expectExceptionMessage('DBエラー');
-
-    //     // メモを共有する為に、リクエストを送信
-    //     $this->post(route('user.share-setting.store'), $requestData);
-    // }
-    // --------------------------------------------------------------------------------------------------------
     /**
      * 自分に共有されたメモの詳細表示が、正しく動作することをテスト
      * @return void
      */
     public function testShowShareSettingController()
     {
-        // 1件の他人のメモを、自分に共有する設定を作成
-        $shareSetting = $this->createSharedMemo($this->secondaryUserMemo);
+        // 1件の共有設定を作成（2人目のユーザーのメモを、自分に共有）
+        $shareSetting = $this->createShareSetting($this->secondaryUser, $this->user);
         // 自分に共有された他人のメモに、2件のタグと画像を関連付け
         [$attachedTags, $attachedImages] = $this->attachTagsAndImages($shareSetting);
 
@@ -205,9 +186,9 @@ class ShareSettingControllerTest extends TestCase
         $response->assertViewIs('user.shareSettings.show');
 
         // ビューに渡されるデータが正しいか確認
-        $response->assertViewHas('select_memo', function ($viewSharedMemo) use ($shareSetting) {
-            // ビューに渡される共有メモのIDが、作成した共有メモのIDと一致することを確認
-            return $viewSharedMemo->id === $shareSetting->memo_id;
+        $response->assertViewHas('select_memo', function ($viewShareSetting) use ($shareSetting) {
+            // ビューに渡される共有メモのIDが、作成した共有設定のmemo_idと一致することを確認
+            return $viewShareSetting->id === $shareSetting->memo_id;
         });
         $response->assertViewHas('get_memo_tags_name', function ($viewSharedTags) use ($attachedTags) {
             // ビューから取得したタグをコレクションに変換
@@ -235,8 +216,8 @@ class ShareSettingControllerTest extends TestCase
      */
     public function testEditShareSettingController()
     {
-        // 1件の他人のメモを、自分に共有する設定を作成
-        $shareSetting = $this->createSharedMemo($this->secondaryUserMemo);
+        // 1件の共有設定を作成（2人目のユーザーのメモを、自分に共有）
+        $shareSetting = $this->createShareSetting($this->secondaryUser, $this->user);
         // 自分に共有された他人のメモに2件のタグと画像を関連付け
         [$attachedTags, $attachedImages] = $this->attachTagsAndImages($shareSetting);
 
@@ -248,9 +229,9 @@ class ShareSettingControllerTest extends TestCase
         $response->assertViewIs('user.shareSettings.edit');
 
         // ビューに渡されるデータが正しいか確認
-        $response->assertViewHas('select_memo', function ($viewSharedMemo) use ($shareSetting) {
+        $response->assertViewHas('select_memo', function ($viewShareSetting) use ($shareSetting) {
             // ビューに渡される共有メモのIDが、作成した共有メモのIDと一致することを確認
-            return $viewSharedMemo->id === $shareSetting->memo_id;
+            return $viewShareSetting->id === $shareSetting->memo_id;
         });
         $response->assertViewHas('get_memo_tags_name', function ($viewSharedTags) use ($attachedTags) {
             // ビューから取得したタグをコレクションに変換
@@ -278,17 +259,19 @@ class ShareSettingControllerTest extends TestCase
      */
     public function testUpdateShareSettingController()
     {
+        // 1件の他人のメモを作成（2人目のユーザー）
+        $memo = $this->createMemo($this->secondaryUser);
         // 2人目の別のユーザーのメモを更新するデータを作成
-        $requestData = ['memoId' => $this->secondaryUserMemo->id, 'content' => '他人のメモの更新テスト',];
+        $requestData = ['memoId' => $memo->id, 'content' => '他人のメモの更新テスト'];
 
         // 共有メモ更新する為に、リクエストを送信
         $response = $this->patch(route('user.share-setting.update'), $requestData);
 
         // 2人目の別のユーザーのメモが更新されたことを確認
         $this->assertDatabaseHas('memos', [
-            'id' => $this->secondaryUserMemo->id,
+            'id' => $memo->id,
             'content' => '他人のメモの更新テスト',
-            'user_id' => $this->secondaryUserMemo->user_id,
+            'user_id' => $this->secondaryUser->id,
         ]);
 
         // レスポンスが 'share-setting.index' リダイレクト先を指していることを確認
@@ -302,10 +285,12 @@ class ShareSettingControllerTest extends TestCase
      */
     public function testDestroyShareSettingController()
     {
+        // 1件の自分のメモを作成
+        $memo = $this->createMemo($this->user);
         // 1件の共有設定のデータを作成（自分のメモを、2人目の別のユーザーに共有）
         $requestData = [
             'share_user_end' => $this->secondaryUser->email,
-            'memoId' => $this->memo->id,
+            'memoId' => $memo->id,
         ];
 
         // 共有したメモを解除する為に、リクエストを送信
@@ -314,7 +299,7 @@ class ShareSettingControllerTest extends TestCase
         // 共有設定が削除されているか確認
         $this->assertDatabaseMissing('share_settings', [
             'sharing_user_id' => $this->secondaryUser->id,
-            'memo_id' => $this->memo->id,
+            'memo_id' => $memo->id,
         ]);
 
         // レスポンスが 'index' リダイレクト先を指していることを確認
